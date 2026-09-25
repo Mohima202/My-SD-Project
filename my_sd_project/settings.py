eacabbbc3650ced4d18b2d9339b6a7fb4c9926bf
Mohima Afroze <afrozmohima@gmail.com>
2026-09-25 17:21:13 +0600

    Temporarily enable debug for Render error

--- my_sd_project/settings.py
+++ my_sd_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-0$&9$r$dbx@$bh4!283#rj%8sjtb$u67pzwg+0pu!__#-d!gys'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['my-sd-project.onrender.com', 'localhost', '127.0.0.1']
 
